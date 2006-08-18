b917f9a42086724158f43a4097e7810f3f73ee75
Miklos Szeredi <miklos@szeredi.hu>
2006-08-18 10:38:37 +0000

    *** empty log message ***

--- generate-faq.sh
+++ generate-faq.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
+
+(
+echo "See http://fuse.sourceforge.net/wiki/index.php/SshfsFaq for the latest"
+echo "version of this FAQ"
+echo "---"
+echo
+
 lynx -nolist -dump http://fuse.sourceforge.net/wiki/index.php/SshfsFaq \
-| sed -e '1,12d' -e '/____/,$d' > FAQ.txt
+| sed -e '1,12d' -e '/____/,$d'
+) > FAQ.txt
