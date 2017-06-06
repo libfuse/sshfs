6c301e043b14db7215d5a8be7d491a981f861ebd
Nikolaus Rath <Nikolaus@rath.org>
2017-06-06 16:28:53 -0400

    Dropped nodelaysrv workaround
    The same effect (enabling NODELAY on the server side *and* enabling
    X11 forwarding) can be achieved by explicitly passing `-o ForwardX11`

--- sshfs.c
+++ sshfs.c
@@ -214,7 +214,6 @@ struct sshfs {
 	struct fuse_args ssh_args;
 	char *workarounds;
 	int rename_workaround;
-	int nodelaysrv_workaround;
 	int truncate_workaround;
 	int buflimit_workaround;
 	int fstat_workaround;
@@ -420,19 +419,15 @@ static struct fuse_opt sshfs_opts[] = {
 
 static struct fuse_opt workaround_opts[] = {
 	SSHFS_OPT("none",       rename_workaround, 0),
-	SSHFS_OPT("none",       nodelaysrv_workaround, 0),
 	SSHFS_OPT("none",       truncate_workaround, 0),
 	SSHFS_OPT("none",       buflimit_workaround, 0),
 	SSHFS_OPT("none",       fstat_workaround, 0),
 	SSHFS_OPT("all",        rename_workaround, 1),
-	SSHFS_OPT("all",        nodelaysrv_workaround, 1),
 	SSHFS_OPT("all",        truncate_workaround, 1),
 	SSHFS_OPT("all",        buflimit_workaround, 1),
 	SSHFS_OPT("all",        fstat_workaround, 1),
 	SSHFS_OPT("rename",     rename_workaround, 1),
 	SSHFS_OPT("norename",   rename_workaround, 0),
-	SSHFS_OPT("nodelaysrv", nodelaysrv_workaround, 1),
-	SSHFS_OPT("nonodelaysrv", nodelaysrv_workaround, 0),
 	SSHFS_OPT("truncate",   truncate_workaround, 1),
 	SSHFS_OPT("notruncate", truncate_workaround, 0),
 	SSHFS_OPT("buflimit",   buflimit_workaround, 1),
@@ -1017,21 +1012,6 @@ static int start_ssh(void)
 		return -1;
 	} else if (pid == 0) {
 		int devnull;
-
-		if (sshfs.nodelaysrv_workaround) {
-			int i;
-			/*
-			 * Hack to work around missing TCP_NODELAY
-			 * setting in sshd
-			 */
-			for (i = 1; i < sshfs.ssh_args.argc; i++) {
-				if (strcmp(sshfs.ssh_args.argv[i], "-x") == 0) {
-					replace_arg(&sshfs.ssh_args.argv[i],
-						    "-X");
-					break;
-				}
-			}
-		}
 
 		devnull = open("/dev/null", O_WRONLY);
 
@@ -3340,7 +3320,6 @@ static void usage(const char *progname)
 "             none             no workarounds enabled\n"
 "             all              all workarounds enabled\n"
 "             [no]rename       fix renaming to existing file (default: off)\n"
-"             [no]nodelaysrv   set nodelay tcp flag in sshd (default: off)\n"
 "             [no]truncate     fix truncate for old servers (default: off)\n"
 "             [no]buflimit     fix buffer fillup bug in server (default: on)\n"
 "    -o idmap=TYPE          user/group ID mapping (default: " IDMAP_DEFAULT ")\n"
@@ -3876,7 +3855,6 @@ int main(int argc, char *argv[])
 	/* SFTP spec says all servers should allow at least 32k I/O */
 	sshfs.max_read = 32768;
 	sshfs.max_write = 32768;
-	sshfs.nodelaysrv_workaround = 0;
 #ifdef __APPLE__
 	sshfs.rename_workaround = 1;
 #else
